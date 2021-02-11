21dafcb429e2763e9c546e5d6e11f6c1ad4dd715
SharadTawde <tawdesharad54@gmail.com>
2021-02-11 21:14:53 +0530

    Initial commit

---
+++ app/src/main/java/com/unicly/deal/ui/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.unicly.deal.ui;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.unicly.deal.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/unicly/deal/ui/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.unicly.deal.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.unicly.deal.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
